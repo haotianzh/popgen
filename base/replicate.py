5b88e98e600a25ddb99ebcbb5a9b7e47ac6a66cd
Haotian <haotianzh@uconn.edu>
2023-05-22 22:18:35 -0400

    small improve

--- base/replicate.py
+++ base/replicate.py
@@ -15,20 +15,26 @@ class Replicate(object):
     #     return genealogies
 
     def genealogies(self, branch_length=False):
-        breakpoints = list(self.ts.breakpoints())
         trees = []
-        for i, tree in enumerate(self.ts.aslist()):
-            if tree.num_roots == 1:
-                trees.append(tree.newick(include_branch_lengths=branch_length))
-            else:
-                trees.append(None)
-        alls = []
         for variant in self.ts.variants():
             pos = variant.position
-            for i in range(len(breakpoints)):
-                if breakpoints[i] < pos <= breakpoints[i + 1]:
-                    alls.append(trees[i])
-        return alls
+            trees.append(self.ts.at(pos))
+
+        # breakpoints = list(self.ts.breakpoints())
+        # for i, tree in enumerate(self.ts.aslist()):
+        #     if tree.num_roots == 1:
+        #         trees.append(tree.newick(include_branch_lengths=branch_length))
+        #     else:
+        #         trees.append(None)
+        # alls = []
+        # for variant in self.ts.variants():
+        #     pos = variant.position
+        #     for i in range(len(breakpoints)):
+        #         if breakpoints[i] < pos <= breakpoints[i + 1]:
+        #             alls.append(trees[i])
+        # return alls
+        return trees
+    
 
     def __str__(self):
         return f'{self.haplotype.nsamples} samples, {self.haplotype.nsites} sites, {self.ts.num_trees} topologies'
