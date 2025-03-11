8cda4df1e48e8fa635b9a3858e771396582db477
Haotian <haotianzh@uconn.edu>
2025-03-11 12:55:16 -0400

    fix a segfault bug when calling np.linalg fucntions

--- utils/__init__.py
+++ utils/__init__.py
@@ -8,9 +8,16 @@ from .utils import *
 from .readers import *
 from .simulator import *
 from .statistics import *
+import numpy as np 
+# not sure why, but calling numpy right before starting JVM is important, otherwise there will be segfault.
+# issue: https://github.com/jpype-project/jpype/issues/808
+# might be the conflicts between openblas and jvm multithreading.
+# set OMP_NUM_THREADS=1 also makes it work.
+tmp = np.linalg.inv(np.random.rand(2400, 2400))  
 # package_dirname = os.path.dirname(popgen.__file__)
 package_dirname = os.path.dirname(os.path.dirname(__file__))
 # start JVM to import .jar libs
+
 if not jpype.isJVMStarted():
     jpype.startJVM(classpath=[os.path.join(package_dirname, 'libs/*')])
 
